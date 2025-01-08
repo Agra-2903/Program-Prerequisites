650e10e518077f66e7cb6678ca61f54664a9fa12
Kushagra Gupta <kushagragupta853@gmail.com>
2025-01-08 10:52:53 +0530

    Area of a Circle program

---
+++ circleArea.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Main{
+ public static void main(String[]args){
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Enter the radius");
+  double radius = sc.nextDouble();
+  System.out.println("Area of the Circle: " + ((22/7) * radius * radius));
+}
+}
